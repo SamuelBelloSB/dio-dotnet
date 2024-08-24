95d55c9f9b846ca50e530cb9dda008dbe58043f3
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-24 19:09:40 -0300

    Para evitar warning CS8618 usei '?' antes da declaração do nome da variavel da classe

--- introducaoDotNet/Models/PessoaFisica.cs
+++ introducaoDotNet/Models/PessoaFisica.cs
@@ -7,7 +7,7 @@ namespace introducaoDotNet.Models
 {
     public class PessoaFisica
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; }
 
         public void Apresentar()
