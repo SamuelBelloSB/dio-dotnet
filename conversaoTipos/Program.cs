5d38bcbf176ac4fc912bd877aa9f83249b414b7b
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-25 15:57:52 -0300

    Estudo sobre Casting, exemplo utilizando conversão para Int usando (int) e Convert.To

---
+++ conversaoTipos/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Conversão para Int usando (int)");
+double numeroDouble = 123.45;
+int numeroInt = (int)numeroDouble; // Cast explícito de double para int
+Console.WriteLine(numeroInt);
+
+long numeroLong = 100000;
+int numeroIntPequeno = (int)numeroLong; // Cast explícito de long para int
+Console.WriteLine(numeroIntPequeno+ "\n");
+
+Console.WriteLine("Conversão para Int usando Convert.ToInt32()");
+double numeroDouble2 = 123.45;
+int numeroInt2 = Convert.ToInt32(numeroDouble2); // Cast explícito de double para int
+Console.WriteLine(numeroInt2);
+
+long numeroLong2 = 100000;
+int numeroIntPequeno2 = Convert.ToInt32(numeroLong2); // Cast explícito de long para int
+Console.WriteLine(numeroIntPequeno2);
