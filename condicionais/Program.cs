71a9f1754d67d503d9780304c29864721f5bb099
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-25 16:18:10 -0300

    Estrutura condicional simples e com if aninhado

---
+++ condicionais/Program.cs
@@ -0,0 +1,11 @@
+﻿int quantidadeEmEstoque = 10;
+int quantidadeDeCompra = 3;
+bool vendaDisponivel = quantidadeEmEstoque >= quantidadeDeCompra;
+
+
+if (quantidadeEmEstoque == 0)
+    Console.WriteLine("Venda indisponível");
+else if (vendaDisponivel) 
+    Console.WriteLine("Venda Realizada");
+else
+    Console.WriteLine("Desculpe, mas não temos a quantidade desejada disponível");
