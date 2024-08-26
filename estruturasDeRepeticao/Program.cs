8c1320e5de5cba7cf827e1758085b6328b3c317b
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-26 16:53:33 -0300

    Uso de repetição com While no c#

--- estruturasDeRepeticao/Program.cs
+++ estruturasDeRepeticao/Program.cs
@@ -4,3 +4,12 @@ for (int contador = 0; contador <= 10; contador++)
 {
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
 }
+
+Console.WriteLine("\nRepetição utilizando While:");
+int numero2 = 5;
+int contador2 = 0;
+while (contador2 <= 10)
+{
+    Console.WriteLine($"{numero2} x {contador2} = {numero2 * contador2}");
+    contador2++;
+}
