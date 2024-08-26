c53d710e4f90b4e91019aaf0fa95b3bc36076bcd
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-26 16:53:14 -0300

    Uso de repetição com for no C#

---
+++ estruturasDeRepeticao/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Repetição utilizando 'For' para tabuada de 5:");
+int numero = 5;
+for (int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
