321b024fcab7a1181a01fd94d79b116181fffd10
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-26 16:00:00 -0300

    Com o uso do int.TryParse eu pude verificar a entrada de dados e converter o valor inserido para int e evitar erros por valor null ou inválidos

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -6,27 +6,34 @@ Console.WriteLine("Digite: 1 , para Somar");
 Console.WriteLine("Digite: 2 , para Subtrair");
 Console.WriteLine("Digite: 3 , para Multiplicar");
 Console.WriteLine("Digite: 4 , para Dividir");
-int opcao = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Digite um valor para x:");
-int x = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite um valor para y:");
-int y = int.Parse(Console.ReadLine());
-switch (opcao)
-{
-    case 1:
-        calculadora_01.Somar(x, y);
-        break;
-    case 2:
-        calculadora_01.Subtrair(x, y);
-        break;
-    case 3:
-        calculadora_01.Multiplicar(x, y);
-        break;
-    case 4:
-        calculadora_01.Dividir(x, y);
-        break;
-    default:
-        Console.WriteLine($"Valor {opcao} é invalido para operação.");
-        break;
+if (int.TryParse(Console.ReadLine(), out int opcao)) {
+    Console.WriteLine("Digite um valor para x:");
+    if (int.TryParse(Console.ReadLine(), out int x)){
+        Console.WriteLine("Digite um valor para y:");
+        if (int.TryParse(Console.ReadLine(), out int y)){
+            switch (opcao)
+            {
+                case 1:
+                    calculadora_01.Somar(x, y);
+                    break;
+                case 2:
+                    calculadora_01.Subtrair(x, y);
+                    break;
+                case 3:
+                    calculadora_01.Multiplicar(x, y);
+                    break;
+                case 4:
+                    calculadora_01.Dividir(x, y);
+                    break;
+                default:
+                    Console.WriteLine($"Valor {opcao} é invalido para operação.");
+                    break;
+            }
+        }
+        else Console.WriteLine("Entrada inválida para y. Por favor, insira um número inteiro");
+    }
+    else Console.WriteLine("Entrada inválida para x. Por favor, insira um número inteiro");  
 }
+else Console.WriteLine("Opção inválida. Por favor, insira um número inteiro de 1 até 4");
+
+
