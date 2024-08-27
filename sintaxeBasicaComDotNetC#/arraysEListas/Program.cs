5ddf1f1bcb5a3e68fc2415ca20e8b89a2958baeb
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-27 16:33:56 -0300

    Estudo sobre Array e Listas, utilizando o For para inserir valores no array

---
+++ sintaxeBasicaComDotNetC#/arraysEListas/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Array
+int[] arrayDeInteiros = new int[5];
+//  Utilizando o For para inserir valores no array
+for (int contador = 0; contador < arrayDeInteiros.Length; contador++)
+{
+    Console.WriteLine($"Insira um valor para a {contador}° posição:");
+    int.TryParse(Console.ReadLine(), out arrayDeInteiros[contador]);
+}
+
