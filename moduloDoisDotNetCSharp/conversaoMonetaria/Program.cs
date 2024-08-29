5d35686e5454a228dd95b6924ca3eaaa3b703718
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-29 17:01:42 -0300

    Comentários sobre o uso do cultureinfo e o :C

--- moduloDoisDotNetCSharp/conversaoMonetaria/Program.cs
+++ moduloDoisDotNetCSharp/conversaoMonetaria/Program.cs
@@ -3,8 +3,9 @@
 Console.WriteLine($"Valor monetário antes de utilizar CultureInfo:");
 decimal valorMonetario = 1450.40M;
 Console.WriteLine($"{valorMonetario:C}");
+// O ":C" serve para inserir a formatação da moeda
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
-
+// O culture info altera a formatação da moeda local para a formatação inserida nos parâmetros, no caso para Americano en-US
 Console.WriteLine($"Valor monetário utilizando CultureInfo 'en-US':");
 Console.WriteLine($"{valorMonetario:C}");
