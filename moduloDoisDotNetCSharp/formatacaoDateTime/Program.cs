646908a7f7136b338cb4bf77c9805a84671e9d3e
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-30 15:22:15 -0300

    Formatando o tipo DateTime, Data e Hora

---
+++ moduloDoisDotNetCSharp/formatacaoDateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+DateTime dataEHoraAtual = DateTime.Now;
+Console.WriteLine($"Data e hora atual: {dataEHoraAtual}");
+
+// Utilizando uma formataçao específica:
+Console.WriteLine($"\nData e hora atual com formatação: {dataEHoraAtual.ToString("dd-MM-yyyy HH:mm")}");
+
+// Utilizando método ToShortDateStrig | Apenas a data.
+Console.WriteLine($"Método ToShortDateString: {dataEHoraAtual.ToShortDateString()}");
+
+// Utilizando método ToShortTimeStrig | Apenas as horas e minutos
+Console.WriteLine($"Método ToShortTimeString: {dataEHoraAtual.ToShortTimeString()}");
+
+// Utilizando Parse para converter String para DateTime
+DateTime convertendoStringParaDateTime = DateTime.Parse("01/06/1998 23:00");
+Console.WriteLine($"Método Parse para converter String em DateTime:{convertendoStringParaDateTime}");
