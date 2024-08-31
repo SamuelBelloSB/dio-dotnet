468f52b11e3e89e5c7b472c9f4e0ad58998d0d27
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-31 12:44:25 -0300

    criando um método que retorna uma exceção utilizando Throw

---
+++ moduloDoisDotNetCSharp/excecoes/Models/ExemploExcecao.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace excecoes.Models
+{
+    public class ExemploExcecao
+    {
+        public void Metodo1()
+        {
+            Metodo2();
+        }
+        public void Metodo2()
+        {
+            Metodo3();
+        }
+        public void Metodo3()
+        {
+            Metodo4();
+        }
+        public void Metodo4()
+        {
+            throw new Exception("Ocorreu uma exceção");
+        }
+    }
+}
