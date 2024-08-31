27b8c866e65f82b4dd46ee042af88ce014fc954d
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-31 15:49:02 -0300

    removendo, alterando e verificando elementos do dicionário

--- moduloDoisDotNetCSharp/dicionarios/Program.cs
+++ moduloDoisDotNetCSharp/dicionarios/Program.cs
@@ -7,8 +7,29 @@ estados.Add(11, "SP");
 estados.Add(12, "SP");
 estados.Add(13, "SP");
 estados.Add(27, "ES");
+Console.WriteLine("Antes:");
+foreach (KeyValuePair<int, string> item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key} | Valor: {item.Value}");
+}
+
+estados.Remove(23);
+estados[21] = "RJ valor alterado";
+Console.WriteLine("\nDepois:");
 
 foreach (KeyValuePair<int, string> item in estados)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Chave: {item.Key} | Valor: {item.Value}");
+}
+
+int chave = 71;
+Console.WriteLine($"\nVerificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Chave já existente.");
+}
+else 
+{
+    Console.WriteLine($"Chave disponível para ser inserida.");
 }
