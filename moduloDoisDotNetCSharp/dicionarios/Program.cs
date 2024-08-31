e760e69af9fae8232cdf13a5aabdb326b42835ab
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-31 15:38:30 -0300

    dicionarios no C#

---
+++ moduloDoisDotNetCSharp/dicionarios/Program.cs
@@ -0,0 +1,14 @@
+﻿Dictionary<int, string> estados = new Dictionary<int, string>();
+
+estados.Add(21, "RJ");
+estados.Add(22, "RJ");
+estados.Add(23, "RJ");
+estados.Add(11, "SP");
+estados.Add(12, "SP");
+estados.Add(13, "SP");
+estados.Add(27, "ES");
+
+foreach (KeyValuePair<int, string> item in estados)
+{
+    Console.WriteLine(item);
+}
