9f2ff39cfd809c4831cf14ad96e7917674384a1e
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-31 15:17:41 -0300

    criando pilha e estudando seus métodos no C#

---
+++ moduloDoisDotNetCSharp/pilhas/Program.cs
@@ -0,0 +1,17 @@
+﻿Stack<int> pilha = new Stack<int>();
+pilha.Push(1);
+pilha.Push(15);
+pilha.Push(5);
+pilha.Push(10);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+pilha.Push(29);
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
