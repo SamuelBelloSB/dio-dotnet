b63e1ace492ba360a6676e07b182930192103df8
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-08-31 18:35:27 -0300

    descartar um retorno específico de um método, descarte.

--- moduloDoisDotNetCSharp/tuplas/Program.cs
+++ moduloDoisDotNetCSharp/tuplas/Program.cs
@@ -6,7 +6,7 @@
 // podemos nomear os itens da tupla para uma semântica melhor
 // como no exemplo: id, nome, sobrenome e altura.
 // */
-
+Console.WriteLine("Formas diferentes de criar tuplas:");
 (int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Samuel", "Bello", 1.77M);
 
 Console.WriteLine($"Id: {tupla.Id}");
@@ -21,12 +21,33 @@ var outroExemploDeTupla = Tuple.Create(1, "Samuel", "Bello", 1.77M);
 // //  Exemplo 2: semelhante ao primeiro, mas sem a possibilidade de nomear os tipos para uma melhor semântica.
 ValueTuple<int, string, string, decimal> outroExemploDeTupla2 = (1, "Samuel", "Bello", 1.77M);
 
+
+//  Utilizando tupla no retorno de métodos:
+Console.WriteLine("\nUtilizando tuplas no returno de métodos tuplas:");
 LeituraArquivo leitura = new LeituraArquivo();
 var (sucesso, linhasArquivo, quantidadeLinhas) = leitura.LerArquivo("Arquivos/arquivoLeitura.txt");
 if (sucesso)
 {
     Console.WriteLine($"Quantidade de linhas no arquivo: {quantidadeLinhas}");
     foreach (var item in linhasArquivo)
+    {
+        Console.WriteLine($"{item}");
+    }
+}
+else
+{
+    Console.WriteLine($"Não possível ler o arquivo.");
+}
+
+
+//  Podemos descartar um dado do retorno da tupla utilizando "_" no lugar a onde seria armazenada
+Console.WriteLine("\nDescartando um dado do retorno de um método:");
+LeituraArquivo leitura2 = new LeituraArquivo();
+var (sucesso2, linhasArquivo2, _) = leitura2.LerArquivo("Arquivos/arquivoLeitura.txt");
+if (sucesso)
+{
+    // Aqui diferente do retorno da primeira tupla, optamos por não usar a quantidade de linhas, então descartamos com "_"
+    foreach (var item in linhasArquivo2)
     {
         Console.WriteLine($"{item}");
     }
